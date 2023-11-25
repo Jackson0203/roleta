dabdb1b4ba895843ca429f8360f5b80c2da92e22
Jackson Santos <jacksonsantos300@outlook.com>
2023-11-25 16:32:52 -0300

    total

---
+++ api/app.py
@@ -0,0 +1,80 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+import threading
+from flask import Flask, render_template
+from datetime import datetime  # Adicione esta linha
+
+app = Flask(__name__, template_folder='templates')
+resultados = []
+ultimo_id_resultado = 0  # Rastreia o último ID de resultado
+
+@app.route('/')
+def index():
+    # Organize os resultados por data
+    resultados_por_data = {}
+    for resultado in resultados:
+        data = resultado['data']
+        if data not in resultados_por_data:
+            resultados_por_data[data] = []
+        resultados_por_data[data].append(resultado['conteudo'])
+
+    return render_template('index.html', resultados_por_data=resultados_por_data)
+
+def coletar_dados():
+    while True:
+        # Obtém a data atual
+        today = datetime.now().strftime("%Y-%m-%d")
+
+        url = "https://casino.betfair.com/pt-br/c/roleta"
+
+        try:
+            servico = Service(ChromeDriverManager().install())
+            chrome_options = Options()
+            chrome_options.add_argument("--headless")
+            driver = webdriver.Chrome(service=servico, options=chrome_options)
+            # Definir um tempo limite para esperar até que o elemento seja encontrado
+            driver.implicitly_wait(10)  # Aguarda até 10 segundos por elementos
+
+            # Fazer a requisição GET para a página
+            driver.get(url)
+
+            while True:
+                try:
+                    roulette_element = driver.find_element(By.XPATH, "//*[@id='root']/div/div[1]/div[1]/div/div[2]/div/div[2]/div/div[1]/div[1]/div/span[1]")
+                    roulette_number = roulette_element.text.strip()
+                    current_time = time.strftime("%H:%M:%S", time.localtime())
+                    resultado = {
+                        'data': today,
+                        'conteudo': f"{current_time}: {roulette_number}"
+                    }
+
+                    # Adicione o resultado à lista de resultados
+                    resultados.append(resultado)
+
+                    # Adicione um log
+                    print(f"Novo resultado coletado: {resultado}")
+
+                    time.sleep(50)  # Aguarda 50 segundos antes de buscar o próximo resultado
+
+                except NoSuchElementException:
+                    print("Elemento da roleta não encontrado")
+                    break
+
+            # Fechar o driver do Selenium
+            driver.quit()
+
+        except Exception as e:
+            print("Ocorreu um erro durante a execução:", str(e))
+
+if __name__ == "__main__":
+    # Crie uma thread para coletar dados
+    thread_coleta = threading.Thread(target=coletar_dados)
+    thread_coleta.start()
+
+    # Inicie o servidor Flask para permitir a comunicação
+    app.run(host='0.0.0.0', port=10000)
